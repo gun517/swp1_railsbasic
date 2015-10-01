fe0fe9265c3e28144809adba6df0a7bc71c64c45
Kun Hyoung <rigel517@gmail.com>
2015-10-01 05:48:38 +0000

    commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,19 @@
+class HomeController < ApplicationController
+    def index
+        
+    end
+    
+    def about
+        
+    end
+    
+    def company
+    end
+
+    def people
+    end
+    
+    def address
+    end
+
+end
